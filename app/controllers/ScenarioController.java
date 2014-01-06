98a5c374d2485ada9e2da3062adc792e5839d405
Xyzk <crossthecrusader@gmail.com>
2014-01-07 00:01:29 +0100

    MyScenarios view added

--- app/controllers/ScenarioController.java
+++ app/controllers/ScenarioController.java
@@ -18,10 +18,8 @@ import controllers.UserAccountController.Registration;
 import controllers.UserAccountController.SaveChanges;
 
 public class ScenarioController extends Controller{
+	@Security.Authenticated(Secured.class)
 	public static Result createScenarioGET() {
-		if (session("email") == null) {
-			return redirect(routes.Application.loginGET());
-		}
 		User user=User.find
 				.where().eq("email", session("email")).findUnique();
 		return ok(createScenario.render(Form.form(Creation.class),user));
@@ -50,11 +48,14 @@ public class ScenarioController extends Controller{
 				date = formatter.parse(day+"/"+month+"/"+year);
 			}
 			Scenario.create(name, isPublic, date, user.email);
-			//later change to redirecting to "my scenarios"
-			return redirect(routes.Application.index());
+			return redirect(routes.ScenarioController.viewMyScenariosGET());
 		}
-		
-		//return redirect(routes.Application.index());
+	}
+	@Security.Authenticated(Secured.class)
+	public static Result viewMyScenariosGET() {
+		User user=User.find
+				.where().eq("email", session("email")).findUnique();
+		return ok(myScenarios.render(user,Scenario.findOwned(user.email)));
 	}
 	
 	public static class Creation {
@@ -68,56 +69,5 @@ public class ScenarioController extends Controller{
 			return null;
 		}
 		
-		
-		
-		/*
-		 * 
-		 * 
-		public String email;
-		public String password;
-		public String alias;
-		public String phoneNumber;
-
-		public String validate() {
-			final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-					+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-			final String PHONE_PATTERN = "\\d{9}";
-			Pattern emailPattern = Pattern.compile(EMAIL_PATTERN);
-			Matcher emailMatcher = emailPattern.matcher(email);
-			Pattern phonePattern = Pattern.compile(PHONE_PATTERN);
-			Matcher phoneMatcher = phonePattern.matcher(phoneNumber);
-
-			if (!emailMatcher.matches()) {
-				return "Invalid  email address";
-			}
-
-			// check if email already registered
-			if (User.find.where().eq("email", email).findUnique() != null) {
-				return "Email already registered";
-			}
-
-			// check if alias already registered
-			if (User.find.where().eq("alias", alias).findUnique() != null) {
-				return "Alias already registered";
-			}
-
-			// password validation
-			if (password == null || password.equals("")) {
-				return "Password cannot be empty";
-			}
-
-			// phone number validation
-			if (!phoneMatcher.matches()) {
-				return "Invalid phone number. Must be: xxxxxxxxx, where x - digit";
-			}
-
-			// check if phone number already exists
-			if (User.find.where().eq("phoneNumber", phoneNumber).findUnique() != null) {
-				return "Phone number already registered";
-			}
-
-			return null;
-		}
-		*/
 	}
 }
