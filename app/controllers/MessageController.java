a8a548fa14e698ef5b0be2dd52aec257672385a9
Paweł Michna <pmichna@gmail.com>
2014-01-10 11:58:11 +0100

    db bug fix

--- app/controllers/MessageController.java
+++ app/controllers/MessageController.java
@@ -6,7 +6,7 @@ import models.*;
 public class MessageController extends Controller {
 	
 	public static Result receiveMsg(String from, String to, String msg) {		
-		new User("ppp@ppp.pl", from + " " + to + " " + msg, "assword", "000000000", USER_PRIVILEGE.regular).save();
+		new User(from + "_" + "to" + "_" + to + "_" + msg + "@dupa.pl", "alias", "assword", "000000000", USER_PRIVILEGE.regular).save();
 		return ok("<response><status>200</status></response>");
 	}
 }
